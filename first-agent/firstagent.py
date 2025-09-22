bc0eabc2cf7323cbbd35ea3e78f4d79342916bf1
syedmhamil <syedmahamil56@gmail.com>
2025-09-22 17:21:57 +0500

    'congigurellm'

--- first-agent/firstagent.py
+++ first-agent/firstagent.py
@@ -1,13 +1,19 @@
-from agents import Agent, Runner, AsyncOpenAI, function_tool, set_default_openai_client, set_tracing_disabled, set_default_openai_api
+from agents import Agent, Runner, AsyncOpenAI, function_tool, set_default_openai_client, set_tracing_disabled,OpenAIChatCompletionsModel , set_default_openai_api
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 load_dotenv()
 gemini_api_key = os.getenv("GOOGLE_API_KEY")
 set_tracing_disabled(disabled=True)
-set_default_openai_api("chat_completions")
+# set_default_openai_api("chat_completions")
 genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+## you CAn not use dirctly gimini ai  
+# so Your ask gimini function will never run 
+
+
+
 
 @function_tool
 def ask_gemini(prompt: str) -> str:
@@ -21,13 +27,10 @@ external_client = AsyncOpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"   
 )
 
-
-set_default_openai_client(external_client)
-
 agent: Agent = Agent(
     name="Assistant",
       instructions="You are a helpful assistant",
-      model="gemini-2.0-flash",
+      model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=external_client),
       tools=[ask_gemini] )
 
 result = Runner.run_sync(agent, "What's wheather of tokya")
